2164343b5518e3abd5388e4570ca2eeabd673d14
Pjatte <91551936+Pjatte1337@users.noreply.github.com>
2022-03-25 00:00:52 +0100

    updated shopingcart

--- js/my-account.js
+++ js/my-account.js
@@ -5,3 +5,14 @@ document.getElementById("button").addEventListener("click", function(){
 document.querySelector(".close").addEventListener("click", function(){
     document.querySelector(".popup").style.display = "none";
 })
+
+
+
+
+document.getElementById("btn").addEventListener("click", function(){
+    document.querySelector(".popups").style.display = "flex";
+})
+
+document.querySelector(".closes").addEventListener("click", function(){
+    document.querySelector(".popups").style.display = "none";
+})
